cba5ae7ad878c46890c63ee22d378f8b4b76284d
Christopher A. Mosher <cmosher01@gmail.com>
2020-06-18 10:08:40 -0400

    enhance up script, and pi

--- up.sh
+++ up.sh
@@ -2,50 +2,54 @@
 
 
 
-# REFERENCED DIRECTORIES/FILES
-# ./mosher.mine.nu (contains main web site)
-# ./images (contains ptif images)
-# ~/.ssh/id_rsa (private key to access github repositories)
-# /etc/letsencrypt (TLS certificates) (use "sudo certbot renew" to renew)
-#
-# MANUAL SETUP:
-# start WinGeneal VM (must be set up with ftm share)
-#   run as root: copy_ftm_media_from_running_vm.sh
-#   to copy media to ./mosher.mine.nu/ftm
+me="$(readlink -f "$0")"
+here="$(dirname "$me")"
+cd "$here"
+
+# nginx doesn't like when upstream servers go down
+docker-compose stop nginx
+sleep 1
+
+
 
 
 
 # stop gedcom and tei containers and remove their data:
 docker-compose stop gedcom-web-view
 docker-compose stop tei
-sleep 3
+sleep 5
 
-docker-compose rm gedcom-web-view
-docker-compose rm fetch-gedcom
+docker-compose rm -f gedcom-web-view
+docker-compose rm -f fetch-gedcom
+docker-compose rm -f tei
+docker-compose rm -f fetch-tei
+sleep 2
+
 docker volume rm -f docker_gedcom
-
-docker-compose rm tei
-docker-compose rm fetch-tei
 docker volume rm -f docker_tei
+
+
+
+
 
 # pull/build latest versions
 docker-compose pull
 docker-compose build
+
+
+
+
 
 # create volumes, network, and containers (but don't start)
 docker-compose up --no-start
 
 
 
-
 docker-compose start fetch-tei fetch-gedcom
-
-
 
 docker-compose start iip uniwebfonts unicode webhook
 
-
-sleep 9
+sleep 5
 docker-compose start tei gedcom-web-view
 
 sleep 9
