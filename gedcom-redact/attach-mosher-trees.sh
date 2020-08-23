6156747618f689bc811ac8bdf23f26c48300a03e
Christopher Alan Mosher <mosher01@gmail.com>
2020-08-23 02:05:35 +0000

    clean up master-skel dir

--- gedcom-redact/attach-mosher-trees.sh
+++ gedcom-redact/attach-mosher-trees.sh
@@ -13,12 +13,14 @@ rm *.ged
 gedcom-uid <$srcdir/root.ged >root.ged
 gedcom-cull root.ged
 gedcom-attach master.ged root.ged.cull
+rm root.ged root.ged.cull
 
 for ged in mosher lovejoy colvin spohner harrison taylorson mclaughlin disosway justice flandreau pettit lopez romero ; do
     echo "Culling: $ged"
     gedcom-uid <$srcdir/$ged.ged >$ged.ged
     gedcom-cull $ged.ged
     gedcom-attach master.ged master.ged $ged.ged.cull
+    rm $ged.ged $ged.ged.cull
 done
 
 ls -lt
