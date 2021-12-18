10e0d7d6bad44dc87d581f74ab89377b7451a202
Christopher A. Mosher <cmosher01@gmail.com>
2021-12-18 00:05:08 -0500

    fix up script for tei2 testing

--- up.sh
+++ up.sh
@@ -14,14 +14,20 @@ sleep 2
 
 # stop tei container and remove its data:
 docker-compose stop tei
+docker-compose stop tei2
 sleep 5
 
 docker-compose rm -f tei
 docker-compose rm -f fetch-tei
+docker-compose rm -f tei2
 sleep 2
 
 docker volume rm -f mosherminenu_tei
-
+docker volume rm -f mosherminenu_archive
+docker volume rm -f mosherminenu_war
+docker volume rm -f mosherminenu_tomcat_logs
+docker volume rm -f mosherminenu_tomcat_work
+docker volume rm -f mosherminenu_tomcat_temp
 
 
 
@@ -37,10 +43,10 @@ docker-compose build
 # create volumes, network, and containers (but don't start)
 docker-compose up --no-start
 
-docker-compose start fetch-tei ftm-web-view iip uniwebfonts unicode
+docker-compose start fetch-tei war archive ftm-web-view iip uniwebfonts unicode
 
 sleep 9
-docker-compose start tei
+docker-compose start tei tei2
 
 sleep 9
 docker-compose start nginx
