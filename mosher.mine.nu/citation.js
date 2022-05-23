4f0580e15e0eb5d1565e51595c9a358dd4c04490
Christopher A. Mosher <cmosher01@gmail.com>
2022-05-23 00:24:49 -0400

    fix monograph date

--- mosher.mine.nu/citation.js
+++ mosher.mine.nu/citation.js
@@ -20,6 +20,12 @@ function doCopy() {
 function ready() {
     var e = document.getElementById("urn:uuid:52595c4e-d8ca-45e8-af3f-396e068adc46");
     e.onclick = doCopy;
+
+    var d = new Date();
+    d = d.toISOString();
+    d = d.substring(0,10);
+    e = document.getElementById("urn:uuid:a3588dc5-a13d-4310-96d3-12b12dac6b6b");
+    e.innerText = d;
 }
 
 document.addEventListener("DOMContentLoaded", ready);
